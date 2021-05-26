534d28b73a9cd1f5524feaf9f297908057b18d01
karl bertin <bertin_k@qwasar.io>
2021-05-26 21:17:54 +0000

    [quest04/] ex01/ updated: finished My STRDUP.

--- ex01/my_strdup.c
+++ ex01/my_strdup.c
@@ -8,7 +8,30 @@
 **
 */
 
-char* my_strdup(char* param_1)
-{
+#include <string.h>
+#include <stdlib.h>
+#include <stddef.h>
+//#include <stdio.h>
 
+char* my_strdup( char* s )
+{
+//	char* s = param_1;
+	int l = strlen(s) + 1;
+	char* s_2 = malloc(l);
+
+	if ( s_2 == NULL )
+		return NULL;
+	(void) memcpy(s_2, s, l);
+	return s_2;
 }
+
+/*int main()
+{
+	char* s_0 = "abc";
+	char* s_1 = "RaInB0";
+	char* s_2 = "";
+	
+	printf("%s\n", s_0);
+	printf("%s\n", s_1);
+	printf("%s\n", s_2);
+}*/
